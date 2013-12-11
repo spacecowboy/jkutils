ce5b9c337e99cb1460677561a0234c73409e3151
Jonas Kalderstam <jonas@kalderstam.se>
2013-12-11 16:58:52 +0100

    Added requirements and install Make target
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@
 from distutils.core import setup, Extension
 
 setup(name = 'jkutils',
-      version = '1.0',
+      version = '1.1',
       description = 'Some utility functions',
       author = 'Jonas Kalderstam',
       author_email = 'jonas@kalderstam.se',
       url = 'https://github.com/spacecowboy/',
       packages = ['jkutils'],
       package_dir = {'jkutils': 'jkutils'},
-      requires = ['numpy'],
+      install_requires = ['scipy>=0.13',
+                          'numpy>=1.7',
+                          'matplotlib>=1.3'],
      )
